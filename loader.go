b6f3cb82cf44a01d64def8346a919fe6781cc8fd
y-du <42994541+y-du@users.noreply.github.com>
2022-09-03 13:18:37 +0200

    add missing basic types

--- loader.go
+++ loader.go
@@ -28,11 +28,49 @@ const tag = "env_var"
 func setField(field reflect.Value, value string) (err error) {
 	if field.IsValid() {
 		var v interface{}
+		var ui uint64
+		var i int64
+		var f float64
+		var c complex128
 		switch field.Kind() {
+		case reflect.Uint:
+			ui, err = strconv.ParseUint(value, 10, 0)
+			v = uint(ui)
+		case reflect.Uint8:
+			ui, err = strconv.ParseUint(value, 10, 8)
+			v = uint8(ui)
+		case reflect.Uint16:
+			ui, err = strconv.ParseUint(value, 10, 16)
+			v = uint16(ui)
+		case reflect.Uint32:
+			ui, err = strconv.ParseUint(value, 10, 32)
+			v = uint32(ui)
+		case reflect.Uint64:
+			v, err = strconv.ParseUint(value, 10, 64)
+		case reflect.Int:
+			i, err = strconv.ParseInt(value, 10, 0)
+			v = int(i)
+		case reflect.Int8:
+			i, err = strconv.ParseInt(value, 10, 8)
+			v = int8(i)
+		case reflect.Int16:
+			i, err = strconv.ParseInt(value, 10, 16)
+			v = int16(i)
+		case reflect.Int32:
+			i, err = strconv.ParseInt(value, 10, 32)
+			v = int32(i)
 		case reflect.Int64:
 			v, err = strconv.ParseInt(value, 10, 64)
+		case reflect.Float32:
+			f, err = strconv.ParseFloat(value, 32)
+			v = float32(f)
 		case reflect.Float64:
 			v, err = strconv.ParseFloat(value, 64)
+		case reflect.Complex64:
+			c, err = strconv.ParseComplex(value, 64)
+			v = complex64(c)
+		case reflect.Complex128:
+			v, err = strconv.ParseComplex(value, 128)
 		case reflect.Bool:
 			v, err = strconv.ParseBool(value)
 		case reflect.Slice, reflect.Map, reflect.Struct:
@@ -42,7 +80,7 @@ func setField(field reflect.Value, value string) (err error) {
 			}
 			field.Set(x.Elem())
 			return
-		default:
+		case reflect.String:
 			v = value
 		}
 		if err != nil {
